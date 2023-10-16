23ae19e3dbc13c868ff28bab0fa958c11fdae10c
ibrivs <134823948+ibrivs@users.noreply.github.com>
2023-10-16 16:56:34 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,45 +1,60 @@
 <?php
-//PUNTO 1
- echo "<h3> Punto 1 </h3>" ;
- $num1 = 19 ;
+//UNO
+echo "<h3> PUNTO A </h3>";
+$numero1 = 1;
 
-if ($num1 > 0) {
-  echo "El numero ingresado es positivo" ;
+while ($numero1 < 10) {
+  echo "El numero es:". $numero1. "<br>";
+  $numero1 ++;
 }
 
-//PUNTO 2
- echo "<h3> Punto 2 </h3>";
- $num2 = 3 ;
- if ($num2 <10 || $num2 >1) {
-  echo "El numero ingresado es mayor a 1 y menor a 10";
- }
+//DOS
+echo "<h3> PUNTO B </h3>";
+$numero2 = 9;
 
-//PUNTO 3
- echo "<h3> Punto  3 </h3>";
- $num3 = 9 ;
+while ($numero2 > 0) {
+  echo "El numero es:" . $numero2 . "<br>";
+  $numero2 -- ;
+}
 
- if ($num3 >= 10 || $num3 <2) {
-   echo "El número ingresado es mayor o igual a 10 o menor a 2";
- } else {
-  echo "El número ingresado no pudo ser validado";
- }
+//tres
+echo "<h3> PUNTO C </h3>";
+$num3 = 1 ;
 
-//PUNTO 4
- echo "<h3> Punto 4 </h3>";
- $numero1 = 8 ;
- $numero2 = 5 ;
+while ($num3 < 20) {
+  $num3++ ;
+  if ($num3 % 2 == 0) {
+    echo "El numero es:" . $num3 . "<br>" ;
+  }
+}
 
- if ($numero1 > $numero2) {
-   $resta = $numero1 - $numero2 ;
-   $suma = $numero1 + $numero2 ;
-   echo "Suma: $suma" . "<br>" . "Resta: $resta" . "<br>";
- } elseif ($numero2 > $numero1) {
-   $mult = $numero1 * $numero2 ;
-   $div = $numero2 / $numero2 ;
-   $resto = $numero2 % $numero1 ;
-   echo "Multiplicacion: $mult" . "<br>" . "Division: $div". "<br>". "Resto: $resto";
- } else {
-   echo "Los numeros ingresados son iguales.";
- }
+//cuatro
+echo "<h3> PUNTO D </h3>";
+$num4 = 1 ;
+$suma = 0 ;
 
+while ($num4 <20) {
+  $num4 ++;
+  if ($num4%2==1) {
+    echo "El numero es:" . $num4 . "<br>";
+  }
+}
+
+//cinco
+echo "<h3> PUNTO E </h3>";
+$suma =0 ;
+
+for ($i=1; $i < 20; $i++) {
+  $suma += $i;
+  echo "El numero es:" . $suma . "<br>";
+}
+
+//seis
+echo "<h3> PUNTO F </h3>";
+$suma2 =0 ;
+
+for ($j=2; $j < 20; $j+= 2) {
+  $suma2 += $j ;
+  echo "El numero es:" . $suma2 . "<br>";
+}
  ?>
